9e61788fdbc05a43a5fd04e45bc9aea710b4005e
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-01-13 15:23:02 +0330

    Delete config.go

--- config.go
+++
@@ -1,9 +0,0 @@
-package config
-func AppConfig() map[string]string {
-	config := make(map[string]string)
-	config["database-address"] = "users.db"
-	config["database-name"] = "users"
-	config["database-username"] = "None"
-	config["database-password"] = "None"
-	return config
-}
